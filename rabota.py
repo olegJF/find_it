43b4c54cb78ed2156fe4f98752f2c1ff1643c69f
oleg <jf2@ua.fm>
2018-05-13 12:29:08 +0300

    Добавили элементы пагинации и дату для urls

--- rabota.py
+++ rabota.py
@@ -2,17 +2,29 @@ import requests
 from bs4 import BeautifulSoup as BS
 import codecs
 import time
+import datetime
 session = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
     }
-base_url = 'https://rabota.ua/jobsearch/vacancy_list?regionId=1&keyWords=python'
+base_url = 'https://rabota.ua/jobsearch/vacancy_list?regionId=1&keyWords=python&period=2&lastdate='
 
 domain = 'https://rabota.ua'
 jobs = []
 urls = []
+yesterday = datetime.date.today()-datetime.timedelta(1)
+one_day_ago = yesterday.strftime('%d.%m.%Y')
+base_url = base_url + one_day_ago
 urls.append(base_url)
-# urls.append(base_url+'&page=2')
+
+req = session.get(base_url, headers=headers)
+if req.status_code == 200:
+    bsObj = BS(req.content, "html.parser")
+    pagination = bsObj.find('dl', attrs={'id': 'content_vacancyList_gridList_pagerInnerTable'})
+    if pagination:
+        pages = pagination.find_all('a', attrs={'class': 'f-always-blue'})
+        for page in pages:
+            urls.append(domain + page['href'])
 
 for url in urls:
     time.sleep(2)
