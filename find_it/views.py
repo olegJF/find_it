5f54ea3b2f3928a2283d90e40e7e6be9f153cdc6
oleg <jf2@ua.fm>
2018-05-25 12:37:14 +0300

    Добавил функцию для сохраниния данных с сайта Джинни в БД проекта

--- find_it/views.py
+++ find_it/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from scraping.utils import *
+from scraping.models import *
+
 
 def home(request):
-    return render(request, 'base.html')
+    jobs = djinni()
+    city = City.objects.get(name='Киев')
+    specialty = Specialty.objects.get(name='Python')
+    v = Vacancy.objects.filter(city=city.id, specialty=specialty.id).values('url')
+    url_list = [i['url'] for i in v]
+    for job in jobs:
+        if job['href'] not in url_list:
+            vacancy = Vacancy(city=city, specialty=specialty, url=job['href'],
+                                title=job['title'], description=job['descript'], company=job['company'])
+            vacancy.save()
+
+    return render(request, 'base.html', {'jobs': jobs})
