6e3c6341fb46a3c954919cb448ccdd4d2d0b48d4
oleg <jf2@ua.fm>
2018-05-27 12:45:21 +0300

    Добавил таблицы с названиями сайтов и урлами для поиска

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import *
 
+class VacancyAdnim(admin.ModelAdmin):
+    class Meta:
+        model = Vacancy
+    list_display = ('title', 'url', 'city', 'specialty', 'timestamp')
+
 admin.site.register(City)
-admin.site.register(Vacancy)
+admin.site.register(Vacancy, VacancyAdnim)
 admin.site.register(Specialty)
+admin.site.register(Site)
+admin.site.register(Url)
--- scraping/models.py
+++ scraping/models.py
@@ -22,6 +22,31 @@ class Specialty(models.Model):
         verbose_name_plural='Специальности'
 
 
+class Site(models.Model):
+    name =  models.CharField(max_length=50, verbose_name='Сайт для поиска')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='Сайт для поиска'
+        verbose_name_plural='Сайты для поиска'
+
+
+class Url(models.Model):
+    city = models.ForeignKey(City, verbose_name='Город', on_delete=models.CASCADE)
+    specialty = models.ForeignKey(Specialty,   verbose_name='Специальность',  on_delete=models.CASCADE)
+    site = models.ForeignKey(Site,   verbose_name='Сайт для поиска',  on_delete=models.CASCADE)
+    url_address = models.CharField(max_length=250, unique=True, verbose_name='Адрес для поиска')
+
+    def __str__(self):
+        return 'Специальность {} в г.{} на сайте {} '.format(self.specialty, self.city, self.site)
+
+    class Meta:
+        verbose_name='Адрес для поиска'
+        verbose_name_plural='Адресы для поиска'
+
+
 class Vacancy(models.Model):
     url = models.CharField(max_length=250, unique=True, verbose_name='Адрес вакансии')
     title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
