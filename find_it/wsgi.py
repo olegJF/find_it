7417b1579c66edf1db67ee2c5851b027372b5129
oleg <jf2@ua.fm>
2018-06-30 12:01:01 +0300

    Deploy on heroku

--- find_it/wsgi.py
+++ find_it/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find_it.settings")
 
 application = get_wsgi_application()
+# Heroku settings for static files
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
