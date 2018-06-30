e59a4de2ad0e8af6fa4ae85c2bfa36f037ec69f6
oleg <jf2@ua.fm>
2018-06-30 12:10:55 +0300

    change production.py forDB  settings1

--- find_it/settings/production.py
+++ find_it/settings/production.py
@@ -82,7 +82,7 @@ except:
     DB_PASSWORD = os.environ.get(DB_PASSWORD)
     DB_HOST = os.environ.get(DB_HOST)
     DB_NAME = os.environ.get(DB_NAME)
-    DB_USER = = os.environ.get(DB_USER)
+    DB_USER = os.environ.get(DB_USER)
 
 DATABASES = {
     'default': {
