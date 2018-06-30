a763757df8da63d89266915c873949d10658a266
oleg <jf2@ua.fm>
2018-06-30 12:20:44 +0300

    change production.py forDB  settings3

--- find_it/settings/production.py
+++ find_it/settings/production.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'find_it.wsgi.application'
 try:
     from find_it.secret import DB_PASSWORD, DB_HOST, DB_NAME, DB_USER
 except:
-    DB_PASSWORD = os.environ.get(DB_PASSWORD)
-    DB_HOST = os.environ.get(DB_HOST)
-    DB_NAME = os.environ.get(DB_NAME)
-    DB_USER = os.environ.get(DB_USER)
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
 
 DATABASES = {
     'default': {
