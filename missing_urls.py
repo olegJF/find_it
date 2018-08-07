460001f36b64f218a61527592006df273c325832
oleg <jf2@ua.fm>
2018-08-07 16:08:36 +0300

    changed e-mail sender

--- missing_urls.py
+++ missing_urls.py
@@ -3,12 +3,19 @@ import logging
 import datetime
 import os
 import requests
+import smtplib
+import time
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 today = datetime.date.today()
 dir = os.path.dirname(os.path.abspath('db.py'))
 path = ''.join([dir, '\\find_it\\secret.py'])
 if os.path.exists(path):
-    from find_it.secret import DB_PASSWORD, DB_HOST, DB_NAME, DB_USER, MAILGUN_KEY, API, ADMIN_EMAIL
+    from find_it.secret import (DB_PASSWORD, DB_HOST, DB_NAME, DB_USER, 
+                                MAILGUN_KEY, API, ADMIN_EMAIL, MAIL_SERVER, 
+                                PASSWORD_AWARD, USER_AWARD, FROM_EMAIL)
 else:
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_HOST = os.environ.get('DB_HOST')
@@ -17,9 +24,16 @@ else:
     MAILGUN_KEY = os.environ.get('MAILGUN_KEY')
     API = os.environ.get('API')
     ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    PASSWORD_AWARD = os.environ.get('PASSWORD_AWARD')
+    USER_AWARD = os.environ.get('USER_AWARD')
+    FROM_EMAIL = os.environ.get('FROM_EMAIL')
     
-FROM_EMAIL = 'noreply@find_it.heroku.com'
-SUBJECT = 'Недостающие урлы {}'.format(today)
+# FROM_EMAIL = 'noreply@jobfinderapp.heroku.com'
+# SUBJECT = 'Недостающие урлы {}'.format(today)
+msg = MIMEMultipart('alternative')
+msg['From'] = 'jobfinderapp.heroku.com <{email}>'.format(email=FROM_EMAIL)
+msg['To'] = ADMIN_EMAIL
 
 try:
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD)
@@ -37,6 +51,10 @@ else:
     sp = {i[0]: i[1] for i in sp_qs}
     mis_urls = []
     cnt = 'На дату {}  отсутствуют урлы для следующих пар:'.format(today)
+    mail = smtplib.SMTP(MAIL_SERVER, 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(USER_AWARD, PASSWORD_AWARD)
     for pair in qs:
         cur.execute("""SELECT * FROM scraping_url  WHERE city_id=%s 
                     AND specialty_id=%s;""", (pair[0], pair[1]))
@@ -46,22 +64,27 @@ else:
     if mis_urls:
         for p in mis_urls:
             cnt += 'город -{}, специальность - {}'.format(p[0], p[1])
-        
-        requests.post(API,  auth=("api", MAILGUN_KEY), data={"from": FROM_EMAIL, "to": ADMIN_EMAIL,
-                            "subject": SUBJECT, "text": cnt})
+        msg['Subject'] = 'Недостающие урлы {}'.format(today)
+        part = MIMEText(cnt, 'text')
+        msg.attach(part)
+        mail.sendmail(FROM_EMAIL, email, msg.as_string())
+        time.sleep(2)
+
     cur.execute("""SELECT data FROM scraping_error WHERE timestamp=%s; """, (today, ))
     err_qs = cur.fetchone()
     if err_qs:
-        SUBJECT = 'Ошибки скрапинга по состоянию на {}'.format(today)
+        msg['Subject'] = 'Ошибки скрапинга по состоянию на {}'.format(today)
         data = err_qs[0]['errors']
         cnt = 'На дату {}  отсутствуют урлы для следующих пар:'.format(today)
         for err in data:
             cnt += 'для url -{}, причина - {}\n'.format(err['href'], err['title'])
             
-        requests.post(API,  auth=("api", MAILGUN_KEY), data={"from": FROM_EMAIL, "to": ADMIN_EMAIL,
-                                "subject": SUBJECT, "text": cnt})
+        part = MIMEText(cnt, 'text')
+        msg.attach(part)
+        mail.sendmail(FROM_EMAIL, email, msg.as_string())
 
 
     conn.commit()
     cur.close()
     conn.close()
+    mail.quit()
--- send_emails.py
+++ send_emails.py
@@ -3,6 +3,11 @@ import logging
 import datetime
 import os
 import requests
+import smtplib
+import time
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 today = datetime.date.today()
 ten_days_ago = datetime.date.today() - datetime.timedelta(10)
@@ -10,7 +15,9 @@ ten_days_ago = datetime.date.today() - datetime.timedelta(10)
 dir = os.path.dirname(os.path.abspath('db.py'))
 path = ''.join([dir, '\\find_it\\secret.py'])
 if os.path.exists(path):
-    from find_it.secret import DB_PASSWORD, DB_HOST, DB_NAME, DB_USER, MAILGUN_KEY, API
+    from find_it.secret import (DB_PASSWORD, DB_HOST, DB_NAME, DB_USER, 
+                                MAILGUN_KEY, API, MAIL_SERVER, 
+                                PASSWORD_AWARD, USER_AWARD, FROM_EMAIL)
 else:
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_HOST = os.environ.get('DB_HOST')
@@ -18,12 +25,19 @@ else:
     DB_USER = os.environ.get('DB_USER')
     MAILGUN_KEY = os.environ.get('MAILGUN_KEY')
     API = os.environ.get('API')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    PASSWORD_AWARD = os.environ.get('PASSWORD_AWARD')
+    USER_AWARD = os.environ.get('USER_AWARD')
+    FROM_EMAIL = os.environ.get('FROM_EMAIL')
 
-FROM_EMAIL = 'noreply@find_it.heroku.com'
-SUBJECT = 'Список вакансий за {}'.format(today)
+# FROM_EMAIL = 'noreply@find_it.heroku.com'
+# SUBJECT = 'Список вакансий за {}'.format(today)
 template = '<!doctype html><html lang="en"><head><meta charset="utf-8"></head><body>'
 end = '</body></html>'
 
+msg = MIMEMultipart('alternative')
+msg['Subject'] = 'Список вакансий за  {}'.format(today)
+msg['From'] = 'Вакансии <{email}>'.format(email=FROM_EMAIL)
 try:
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD)
 except:
@@ -32,6 +46,10 @@ else:
     cur = conn.cursor()
     cur.execute(""" SELECT city_id, specialty_id FROM subscribers_subscriber WHERE is_active=%s;""", (True,))
     cities_qs = cur.fetchall()
+    mail = smtplib.SMTP(MAIL_SERVER, 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(USER_AWARD, PASSWORD_AWARD)
     for pair in cities_qs:
         content = ''
         city = pair[0]
@@ -51,12 +69,22 @@ else:
                 content += '<p>{}</p><br/>'.format(job[3])
                 content += '<hr/><br/><br/>'
             html_m = template + content + end
+            part = MIMEText(html_message, 'html')
+            msg.attach(part)
             for email in emails:
-                requests.post(API,  auth=("api", MAILGUN_KEY), data={"from": FROM_EMAIL, "to": email,
-                                "subject": SUBJECT, "html": html_m})
+                msg['To'] = email
+                mail.sendmail(FROM_EMAIL, email, msg.as_string())
+                time.sleep(2)
         else:
-            requests.post(API,  auth=("api", MAILGUN_KEY), data={"from": FROM_EMAIL, "to": email,
-                                "subject": SUBJECT, "text": 'Список вакансий по Вашему профилю на сегодня пуст.'})
+            text = 'На сегодня, список вакансий по Вашему запросу, пуст.'
+            part = MIMEText(text, 'plain')
+            msg.attach(part)
+            for email in emails:
+                msg['To'] = email
+                mail.sendmail(FROM_EMAIL, email, msg.as_string())
+                time.sleep(2)
+
     conn.commit()
     cur.close()
     conn.close()
+    mail.quit()
