617ff76c79f5cb4c83f2921db2662641e52ed2b8
Oleg Novikov <jf2@ua.fm>
2018-09-18 13:44:35 +0300

    Сделал вызов функций скрапинга в цикле

--- db.py
+++ db.py
@@ -6,6 +6,8 @@ import json
 from scraping.utils import *
 
 today = datetime.date.today()
+UTILS_FUNC = [(djinni, 'Djinni.co'), (work, 'Work.ua'),
+            (rabota, 'Rabota.ua'), (dou, 'Dou.ua')]
 ten_days_ago = datetime.date.today() - datetime.timedelta(10)
 dir = os.path.dirname(os.path.abspath('db.py'))
 path = ''.join([dir, '\\find_it\\secret.py'])
@@ -18,12 +20,14 @@ else:
     DB_USER = os.environ.get('DB_USER')
  
 try:
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD)
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, host=DB_HOST, 
+                                password=DB_PASSWORD)
 except:
     logging.exception('Unable to open DB -{}'.format(today))
 else:
     cur = conn.cursor()
-    cur.execute(""" SELECT city_id, specialty_id FROM subscribers_subscriber WHERE is_active=%s;""", (True,))
+    cur.execute(""" SELECT city_id, specialty_id FROM subscribers_subscriber 
+                    WHERE is_active=%s;""", (True,))
     cities_qs = cur.fetchall()
     # print(cities_qs)
     todo_list = {i[0]:set() for i in cities_qs}
@@ -56,18 +60,10 @@ else:
         for url in url_list:
             tmp = {}
             tmp_content = []
-            j, e = djinni(url.get('Djinni.co', None))
-            tmp_content.extend(j)
-            errors.extend(e)
-            j, e = rabota(url.get('Rabota.ua', None))
-            tmp_content.extend(j)
-            errors.extend(e)
-            j, e = work(url.get('Work.ua', None))
-            tmp_content.extend(j)
-            errors.extend(e)
-            j, e = dou(url.get('Dou.ua', None))
-            tmp_content.extend(j)
-            errors.extend(e)
+            for (func, key) in UTILS_FUNC:
+                j, e = func(url[key])
+                tmp_content.extend(j)
+                errors.extend(e)
             tmp['city'] = url['city']
             tmp['specialty'] = url['specialty']
             tmp['content'] = tmp_content
@@ -79,27 +75,34 @@ else:
             specialty = data['specialty']
             jobs = data['content'] 
             for job in jobs:
-                cur.execute("""SELECT * FROM scraping_vacancy WHERE url=%s; """, (job['href'], ))
+                cur.execute("""SELECT * FROM scraping_vacancy 
+                                WHERE url=%s; """, (job['href'], ))
                 qs = cur.fetchone()
                 if not qs:
-                    cur.execute("""INSERT INTO scraping_vacancy (city_id, specialty_id, title,
-                                 url, description, company, timestamp) VALUES (%s, %s, %s, %s, %s, %s, %s); """, 
-                                 (city, specialty, job['title'],job['href'], job['descript'], job['company'], today ))
+                    cur.execute("""INSERT INTO scraping_vacancy 
+                                    (city_id, specialty_id, title,
+                                    url, description, company, timestamp) 
+                                    VALUES (%s, %s, %s, %s, %s, %s, %s); """, 
+                                 (city, specialty, job['title'],job['href'], 
+                                 job['descript'], job['company'], today ))
 
     if errors:
-        cur.execute("""SELECT data FROM scraping_error WHERE timestamp=%s; """, (today, ))
+        cur.execute("""SELECT data FROM scraping_error 
+                        WHERE timestamp=%s; """, (today, ))
         err_qs = cur.fetchone()
         if err_qs:
             data = err_qs[0]
             data['errors'].extend(errors)
-            cur.execute("""UPDATE scraping_error SET data=%s WHERE timestamp=%s; """, (json.dumps(data), today, ))
+            cur.execute("""UPDATE scraping_error SET data=%s 
+                            WHERE timestamp=%s;""", (json.dumps(data), today,))
         else:
             data = {}
             data['errors'] = errors
-            cur.execute("""INSERT INTO scraping_error (data, timestamp) VALUES (%s, %s); """, 
-                                 (json.dumps(data), today ))
+            cur.execute("""INSERT INTO scraping_error (data, timestamp) 
+                            VALUES (%s, %s); """, (json.dumps(data), today ))
                                  
-    cur.execute("""DELETE FROM  scraping_vacancy WHERE timestamp<=%s;""", (ten_days_ago,))
+    cur.execute("""DELETE FROM  scraping_vacancy WHERE timestamp<=%s;""", 
+                    (ten_days_ago,))
 
     conn.commit()
     cur.close()
