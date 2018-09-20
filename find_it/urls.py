53cbf3f46d2d42fbcbdcf20a35911b71819b212c
Oleg Novikov <jf2@ua.fm>
2018-09-20 16:48:05 +0300

    Добавил токен в api-адрес

--- find_it/urls.py
+++ find_it/urls.py
@@ -1,26 +1,13 @@
-"""find_it URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from scraping.views import *
 from subscribers.views import *
 
+TOKEN = os.environ.get('TOKEN')
+
 urlpatterns = [
     url(r'^adminka/', admin.site.urls),
-    url(r'^api/', include('scraping.api.urls')),
+    url(r'^api/{}/'.format(TOKEN ), include('scraping.api.urls')),
     url(r'^list/', vacancy_list, name='list'),
     url(r'^login/', login_subscriber, name='login'),
     url(r'^update/', update_subscriber, name='update'),
