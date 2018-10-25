87bc01ba1dd83dffc2710e44b254e2310d80ea59
Oleg Novikov <jf2@ua.fm>
2018-10-25 13:33:19 +0300

    changed form for new user

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -7,11 +7,16 @@ class SubscriberModelForm(forms.ModelForm):
     city = forms.ModelChoiceField( label='Город', queryset=City.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
     specialty = forms.ModelChoiceField( label='Специальность', queryset=Specialty.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    is_active = forms.BooleanField(label='''Я согласен получать рассылку 
+                                    по вакансиям, на указанный мной e-mail''', 
+                                    required=True, widget=forms.CheckboxInput(
+                                    attrs={'class': 'form-check-input', 
+                                    'checked': "checked"}))
     
     class Meta:
         model = Subscriber
-        fields = ('email', 'city', 'specialty', 'password')
-        exclude = ('is_active',)
+        fields = ('email', 'city', 'specialty', 'password', 'is_active')
+        
 
 class LogInForm(forms.Form):
     email = forms.EmailField(label='E-mail', required=True, widget=forms.EmailInput(attrs={"class": 'form-control'}))
